0e23db6df835fa58df5e83d930e3b9106e0e24bc
aarcanj0 <66854515+aarcanj0@users.noreply.github.com>
2023-08-29 03:38:24 -0300

    Add files via upload

---
+++ src/superm/mainClass.java
@@ -0,0 +1,16 @@
+package superm;
+
+public class mainClass {
+
+    public static void main(String[] args) {
+        System.out.println("App started sucessfull");
+        System.out.println("Java(TM) SE Runtime Environment (build 20.0.2+9-78)");
+        
+        //Show loginWindow
+        loginWindow lw = new loginWindow();
+        lw.show();
+        
+        
+    }
+    
+}
