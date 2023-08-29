046aeafbd3b0d0ac26e7e05657a3a9dc79aa452f
aarcanj0 <66854515+aarcanj0@users.noreply.github.com>
2023-08-29 03:45:30 -0300

    Add files via upload

---
+++ src/superm/passwordCredential.java
@@ -0,0 +1,26 @@
+package superm;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class passwordCredential {
+    public static String calculateMD5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                String hex = Integer.toHexString(0xFF & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
